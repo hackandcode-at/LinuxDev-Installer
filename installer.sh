5b895378aac665348e6a8c8507d897fabf5c4555
MarkusS <hemanmarkus@hotmail.com>
2019-04-14 18:44:28 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,21 @@
+apt install snap
+apt install nodejs
+apt install npm
+apt install python3
+apt install python
+apt install python-pip
+apt install python3-pip
+apt install mono-mcs
+apt install openjdk-11-jre
+apt install openjdk-11-jdk
+apt install apache2
+apt install php
+apt install mysql
+apt install git
+chmod +777 /var/www/html/
+snap install vscode
+snap install atom
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
+apt-get update
+apt-get install virtualbox-6.0
