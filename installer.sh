cd26f7d72b250ae119e22c7e39df38e1c97f91c9
MarkusS <markusscoding@outlook.com>
2020-02-07 13:00:03 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -45,6 +45,7 @@ apt -y install aircrack-ng
 apt -y install ettercap-graphical
 apt -y install netdiscover
 apt -y install crunch
+apt -y install cewl
 snap install zaproxy --classic
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
 git clone https://github.com/trustedsec/social-engineer-toolkit
