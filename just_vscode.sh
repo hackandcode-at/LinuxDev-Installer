c6355821586c3017db153b8292863271f43b54a5
MarkusS <markusscoding@outlook.com>
2021-02-03 00:12:40 +0100

    Update just_vscode.sh

--- just_vscode.sh
+++ just_vscode.sh
@@ -32,6 +32,7 @@ code --install-extension 2gua.rainbow-brackets
 code --install-extension 4tron.stack-overflow-view
 code --install-extension techer.open-in-browser
 code --install-extension cweijan.vscode-ssh
+code --install-extension mkloubert.vscode-kanban
 #this one is just for fun, delete if you want :)
 code --install-extension bar9.stories
 code --install-extension benawad.vsinder
