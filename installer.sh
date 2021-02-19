1ca090376956e77ae5aebf6081b9206ccb8ef6d2
MarkusS <markusscoding@outlook.com>
2021-02-19 09:51:50 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -199,33 +199,13 @@ if [ $install=true ]; then
     chmod +755 /var/www/html/
     snap install gitkraken --classic
     snap install code --classic
-    code --install-extension alefragnani.project-manager
-    code --install-extension christian-kohler.path-intellisense
-    code --install-extension codezombiech.gitignore
-    code --install-extension donjayamanne.git-extension-pack
-    code --install-extension donjayamanne.githistory
-    code --install-extension eamodio.gitlens
-    code --install-extension Equinusocio.vsc-material-theme
-    code --install-extension GitLab.gitlab-workflow
-    code --install-extension mongodb.mongodb-vscode
-    code --install-extension ms-azuretools.vscode-docker
-    code --install-extension ms-vscode-remote.remote-containers
-    code --install-extension ms-vscode-remote.remote-ssh
-    code --install-extension ms-vscode-remote.remote-ssh-edit
-    code --install-extension ms-vscode.cpptools
-    code --install-extension mtxr.sqltools
-    code --install-extension PKief.material-icon-theme
-    code --install-extension platformio.platformio-ide
-    code --install-extension tombonnike.vscode-status-bar-format-toggle
-    code --install-extension vscodevim.vim
-    code --install-extension Wscats.eno
-    code --install-extension zhuangtongfa.material-theme
-    code --install-extension ziyasal.vscode-open-in-github
-    code --install-extension formulahendry.code-runner
+    chmod +x just_vscode.sh
+    ./just_vscode.sh
     snap install kotlin --classic
     snap install slack --classic
     snap install zaproxy --classic
     snap install go --classic
+    snap install dotnet-sdk --classic
     curl -L https://www.npmjs.com/install.sh | sh
     npm install -g sass
     npm install -g typescript
