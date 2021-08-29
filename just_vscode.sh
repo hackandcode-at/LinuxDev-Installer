0bc606bce58dfddadb3df13123f2817f54cad7e5
MarkusS <markusscoding@outlook.com>
2021-08-29 19:04:16 +0200

    Update just_vscode.sh

--- just_vscode.sh
+++ just_vscode.sh
@@ -44,6 +44,7 @@ code --install-extension ritwickdey.liveserver
 code --install-extension github.vscode-pull-request-github
 code --install-extension usernamehw.errorlens
 code --install-extension rohinivsenthil.postcode
+code --install-extension jasonnutter.search-node-modules
 #this one is just for fun, delete if you want :)
 code --install-extension bar9.stories
 code --install-extension benawad.vsinder
